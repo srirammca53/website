6ba1159048573956776435f81312d9ca8395778c
sriram ilapakurty <sriram.in20@gmail.com>
2012-06-14 17:57:33 +0530

    user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Devise::Application.routes.draw do
+  resources :articles
+
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -50,8 +52,8 @@ Devise::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-root :to => redirect("/users/sign_up")
+ root :to => 'articles#index'
+#root :to => redirect("/users/sign_up")
 
   # See how all your routes lay out with "rake routes"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120614114315) do
+ActiveRecord::Schema.define(:version => 20120614121236) do
+
+  create_table "articles", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
